de70d55ae6cbd6f6a484f2a3a735c607b7edc7b9
AntoniusBolt <antonius.bolt@gmail.com>
2024-08-25 15:39:07 -0700

    Add files via upload

---
+++ js/mobile-nav.js
@@ -0,0 +1,16 @@
+/* navigation javascript for mobile screen */
+let headerMain = document.querySelector(".header-main");
+let headerMobileBtn = document.querySelector(".mobile-menu-btn");
+
+let isMenuOpen = false;
+
+headerMobileBtn.onclick = function() {
+	if (!isMenuOpen) { /* not open */
+		headerMain.style.display = "flex";
+		isMenuOpen = true;
+	}
+	else if (isMenuOpen) {
+		headerMain.style.display = "none";
+		isMenuOpen = false;
+	}
+}
